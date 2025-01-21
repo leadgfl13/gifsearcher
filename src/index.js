de68e1b1b43ad0a460923fddf44315d4096d0d20
leadgfl13 <leadgfl13@gmail.com>
2025-01-20 21:29:49 -0500

    Add basic code from odin project to send a fetch request to giphy using API key

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./style.css";
 
-document.addEventListener("click", () => {
-	alert("Hello");
+const img = document.querySelector("img");
+fetch(
+	"https://api.giphy.com/v1/gifs/translate?api_key=SPSm69XvEdKnNRD7THW8YTohV73lYnuW&s=cats"
+).then(function (response) {
+	console.log(response.json());
 });
