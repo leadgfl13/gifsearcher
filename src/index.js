1069b5389f13b6e19d48951bfaf4f1d94af19b3d
leadgfl13 <leadgfl13@gmail.com>
2025-01-20 21:39:48 -0500

    Access the response data of the fetch request

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import "./style.css";
 const img = document.querySelector("img");
 fetch(
 	"https://api.giphy.com/v1/gifs/translate?api_key=SPSm69XvEdKnNRD7THW8YTohV73lYnuW&s=cats"
-).then(function (response) {
-	console.log(response.json());
-});
+)
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (response) {
+		console.log(response.data.images.original.url);
+	});
+let gif = document.getElementById("dispalygif");
+gif.innerHTML = img.url;
