e570c1f742523b4b242315415c00b52ab14af160
leadgfl13 <leadgfl13@gmail.com>
2025-01-22 20:38:51 -0500

    refacture code to use asyn function and await

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import "./style.css";
 
-const img = document.querySelector("img");
-
 let searchinput = document.getElementById("searchbox");
 
 let searchbutton = document.getElementById("searchbutton");
-let request = "";
-
 searchbutton.addEventListener("click", () => {
-	request = searchinput.value;
-	fetch(
-		`https://api.giphy.com/v1/gifs/translate?api_key=SPSm69XvEdKnNRD7THW8YTohV73lYnuW&s=${request}`
-	)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (response) {
-			img.src = response.data.images.original.url;
-			img === img.url;
-			searchinput.value = "";
-		});
+	getGifs();
 });
 
+async function getGifs() {
+	const img = document.querySelector("img");
+	let request = searchinput.value;
+	const response = await fetch(
+		`https://api.giphy.com/v1/gifs/translate?api_key=SPSm69XvEdKnNRD7THW8YTohV73lYnuW&s=${request}`
+	);
+	response.json().then(function (response) {
+		img.src = response.data.images.original.url;
+	});
+	const catData = await response.json();
+	img.src = catData.data.images.original.url;
+}
 //eed a static image to display at first
 
 let gif = document.getElementById("dispalygif");
